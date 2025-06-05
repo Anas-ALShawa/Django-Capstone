adcd3f02a8185bddaa3ac7492796adeead7b6313
anas <anas-shawa2025@hotmail.com>
2025-06-05 11:19:34 +0200

    final-commit

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 router = DefaultRouter()
 router.register(r'tables', views.BookingViewSet)
 urlpatterns = [
+    path('home', views.home),
+    path('api-token-auth/', obtain_auth_token),
     path('restaurant/booking/', include(router.urls)),
     path('menu', views.MenuItemView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
